bcef902c3b50d0a90713fbeca5b3c722c9c145c3
MBrvd <mbrevard@hotmail.fr>
2025-08-08 12:54:25 +0000

    first version of the grimoria app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,45 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.title("🔥💧GRIMORIA 🌪️🌱")
+st.markdown("## Le grimoire en ligne des animaux élémentaires")
+
+c1 = st.container(border=True)
+element = c1.selectbox("Choisis ton élément", 
+                       options=["","🔥 Feu", "💧 Eau", "🌪️ Vent", "🌱 Terre"])
+
+text = "ABCDEFGHIJ"
+
+def get_text_from_animal(offset:int):
+    output = ""
+    for i in range(offset,len(text), 4):
+        output += text[i]
+    return output
+
+if element:
+    c2 = st.container(border=True)
+    animal = c2.text_input("Rentre ton animal élémentaire")
+    validated = c2.button("Valider")
+    output = ""
+    if validated :
+        if element == "🔥 Feu" and animal.upper() == "SALAMANDRE":
+            output = get_text_from_animal(0)
+            color = "red"
+        elif element == "💧 Eau" and animal.upper() == "HIPPOCAMPE":
+            output = get_text_from_animal(1)
+            color = "blue"
+        elif element == "🌪️ Vent" and animal.upper() == "HIRONDELLE":
+            output = get_text_from_animal(2)
+            color = "green"
+        elif element == "🌱 Terre" and animal.upper() == "PHACOCHERE":
+            output = get_text_from_animal(3)
+            color = "grey"
+        elif len(animal) != 10 and len(animal) != 0 :
+            c2.error("Nombre de lettres invalide")
+        else:
+            c2.error("Code incorrect")
+
+    if output : 
+        c3 = st.container(border=True)
+        c3.write("Code correct ! Voici ton prochain indice :")
+        c3.write(f":{color}[{output}]")
